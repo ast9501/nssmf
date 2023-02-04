e910600b6e02f8c21683ac6076e1fbd19e18267a
ast9501 <ast9501.cs10g@nctu.edu.tw>
2023-02-04 09:51:03 +0000

    FIx: import package

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,7 @@ package main
 import (
 	"os"
 
-	//"github.com/ast9501/nssmf/internal/service/management"
-	//nssmf_service "github.com/ast9501/nssmf/pkg/service"
+	nssmf_service "github.com/ast9501/nssmf/pkg/service"
 	"github.com/urfave/cli/v2"
 )
 
@@ -17,12 +16,13 @@ func main() {
 	app.Action = action
 	//app.Flags = NSSF.GetCliCmd()
 	if err := app.Run(os.Args); err != nil {
-		logger.AppLog.Errorf("NSSMF Run Error: %v\n", err)
+		print("Errpr in args")
+		//logger.AppLog.Errorf("NSSMF Run Error: %v\n", err)
 	}
 }
 
 func action(c *cli.Context) error {
-	NSSF.Start()
+	NSSMF.Start()
 
 	return nil
 }
