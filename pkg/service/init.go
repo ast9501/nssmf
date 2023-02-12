3f74b293943a3f95bd32c63fd788d54da749a415
ast9501 <ast9501.cs10g@nctu.edu.tw>
2023-02-12 09:15:54 +0000

    Func: update router to use TLS

--- pkg/service/init.go
+++ pkg/service/init.go
@@ -10,18 +10,6 @@ import (
 type NSSMF struct {
 }
 
-//	@title			O-RAN NSSMF api doc
-//	@version		1.0
-//	@description	NSSMF api doc
-
-//	@contact.name	ast9501
-//	@contact.email	ast9501.cs10@nycu.edu.tw
-
-//	@license.name	Apache 2.0
-//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
-
-//	@host			192.168.0.166:8000
-//
 // schemes http
 func (nssmf *NSSMF) Start() {
 	// TODO: external function call for customize gin engine loger
@@ -34,5 +22,5 @@ func (nssmf *NSSMF) Start() {
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	// TODO: Load server binding port from conifg
-	router.Run(":8000")
+	router.RunTLS(":8000", "../config/TLS/nssmf.nycu.crt", "../config/TLS/nssmf.nycu.key")
 }
