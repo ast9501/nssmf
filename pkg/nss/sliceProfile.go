6c77b9a66a32023d58ffafc583fdaea1384b8240
ast9501 <ast9501.cs10g@nctu.edu.tw>
2023-02-22 18:28:36 +0000

    Func: define data struct for NSS profile

--- pkg/nss/sliceProfile.go
+++ pkg/nss/sliceProfile.go
@@ -2,6 +2,27 @@ package nss
 
 // swagger:model	NetworkSliceSubnetProfileReq
 type NetworkSliceSubnetProfileReq struct {
-	Sst uint64 `json: "sst"`
-	Sd  uint64 `json: "sd"`
+	SliceProfileId string
+	SNssaiList     SNssai
+	PlmnIdList     PlmnId
+	PerfReq        PerReq
+}
+
+type SNssai struct {
+	SSt string `json: "sst"` // 8 bit
+	Sd  string `json: "sd"`  // 24 bit
+}
+
+type PlmnId struct {
+	Mnc string `json: "mnc"` // 3 digit
+	Mcc string `json: "mcc"` // 3 digit
+}
+
+// eMBB PerfReq
+type PerReq struct {
+	ExpDataRateDl      int
+	ExpDataRateUl      int
+	AreaTrafficCapDl   int
+	AreaTrafficCapUl   int
+	OverallUserDensity int
 }
