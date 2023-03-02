cc38d8f36ceb295dbcf5ca27d18ae4fcb1b19137
ast9501 <ast9501.cs10g@nctu.edu.tw>
2023-03-02 09:04:08 +0000

    Func: init network service module

---
+++ pkg/nservice/serviceProfile.go
@@ -0,0 +1,26 @@
+package nservice
+
+func CreateNetworkService() {
+	// create table
+
+	// create related vnf
+}
+
+func UpdateNetworkService() {
+	// update table
+
+	// create/delete vnf
+}
+
+func DeleteNetworkService() {
+	// update table
+	// delete vnf
+}
+
+func GetAllNetworkService() {
+	// query table
+}
+
+func GetNetworkService() {
+	// query table
+}
